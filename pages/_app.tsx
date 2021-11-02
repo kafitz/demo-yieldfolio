c1e303f966b84347232fcab0b38b46bbb97d2f30
K Fitzsimmons <kafitz22@gmail.com>
2021-11-02 16:26:12 -0400

    Add title to yieldfolio pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,25 @@
 
 import React from 'react';
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 import { Provider } from 'react-redux';
 
 import { store } from '../lib/store';
 import '../styles/globals.css'
 
 
-// Intialized NextJS React app with Redux-Toolkit (TODO: remove?)
 function App({ Component, pageProps }: AppProps) {
     return (
 		<React.StrictMode>
 			<Provider store={store}>
+				<Head>
+					<meta
+						name='viewport'
+						content='width=device-width, initial-scale=1, shrink-to-fit=no'
+					/>
+					<title>Yieldfolio - Demo Inc.</title>
+				</Head>
+
 				<Component {...pageProps} />
 			</Provider>
 		</React.StrictMode>
